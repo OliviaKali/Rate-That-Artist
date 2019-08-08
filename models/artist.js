f1e7281d79a6d2912498d62f3feb34e2ce3dcc64
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-08 18:52:36 -0400

    Patrick's stuff

---
+++ models/artist.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  var Artist = sequelize.define("Artist", {
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    artistPic: {
+      type: DataTypes.STRING
+    }
+
+  });
+
+  Artist.associate = function(models) {
+    Artist.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Artist;
+};
