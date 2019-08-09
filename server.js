21854c2e572a90ff12018832ec166434858c2d94
OKali <o.kalinowski27@gmail.com>
2019-08-09 10:28:46 -0400

    Remove unneccessary code

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-require("dotenv").config();
-var keys = require("./keys.js"); //don't know if i need this since its for the front-end
+// require
 
-var express = require('express');
-var app = express();
-var fetch = require('node-fetch');
+// router.get("/api/artists", function(req, res) {
+
+//     function searchArtist() {
+//       var spotify = new Spotify(keys.spotify);
+    
+//       spotify
+//         .search({ type: "artist", query: "blink-182", limit: 3 })
+//         .then(function(response) {
+//           var artistName = response.artists.items[0].name;
+//           var artistGenres = response.artists.items[0].genres;
+//           var imageUrl = response.artists.items[0].images[0].url;
+//           var artistID = response.artists.items[0].id;
+    
+//           console.log("Artist: " + artistName);
+//           console.log("Genre: " + artistGenres);
+//           console.log("imageUrl: " + imageUrl);
+//           console.log("Artist ID #: " + artistID);
+          
+//         })
+//         .catch(function(err) {
+//           console.log(err);
+//         });
+//     }
+    
+//     searchArtist();
+//   });
+  
+//   app.listen(PORT, function() {
+//     console.log("Server listening on: http://localhost:" + PORT);
+//   });
